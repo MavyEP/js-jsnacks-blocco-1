79ae2359ebc6ebfa9e1e60fe710dfec36b9d3aee
edoardo <mavyofficialep@gmail.com>
2020-04-23 19:57:14 +0200

    Fine snack6

---
+++ JSnack 6/main.js
@@ -0,0 +1,9 @@
+
+//creare un ciclo for fino a 1000 per poi andare a fare la potenza su tutti i suoi numeri.
+for (var i = 0; i < 1001; i++) {
+     //variabile contenente i numeri
+     var numero = i ;
+     //variabile per la potenza dei numeri
+     var potenza_di_numero = numero * numero;
+     console.log("Calcola la potenza del numero : " + numero + " = " + potenza_di_numero)
+}
